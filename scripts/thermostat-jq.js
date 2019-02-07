73b922702e669c3bdf9453ce3168111d814c5214
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-07 21:29:43 +0000

    displays temp for chosen cities around the world in a dropdown menu

--- scripts/thermostat-jq.js
+++ scripts/thermostat-jq.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
 
   var thermostat = new Thermostat;
-  var weatherAPIUrl = 'http://api.openweathermap.org/data/2.5/forecast?APPID=9a27568ed1d7f9682ac8dc1b08ea36a0&id='
 
   function getCityURL(city, callback) {
-    $.getJSON( weatherAPIUrl + city, callback);
+    var weatherAPIUrl = 'http://api.openweathermap.org/data/2.5/forecast?'
+    var apiKey = 'APPID=9a27568ed1d7f9682ac8dc1b08ea36a0'
+    var id = '&id='
+    var units = '&units=metric'
+    $.get(weatherAPIUrl + apiKey + units + id + city, callback);
   }
-  getCityURL('2643741', (data) => ($('.day1').text(Math.round(data.list[0].main.temp - 273.15)),
-    $('.day2').text(Math.round(data.list[1].main.temp - 273.15)),
-    $('.day3').text(Math.round(data.list[2].main.temp - 273.15)),
-    $('.day4').text(Math.round(data.list[3].main.temp - 273.15)),
-    $('.day5').text(Math.round(data.list[4].main.temp - 273.15)),
-    $('.day6').text(Math.round(data.list[5].main.temp - 273.15)),
-    $('.day7').text(Math.round(data.list[6].main.temp - 273.15))));
 
 
   function energyUsageColour() {
@@ -24,8 +20,12 @@ $(document).ready(function() {
 
   function printTempAndUsage() {
     $('.temp').text(thermostat.temp).hide(200).show(200);
-    $('.usage').text(thermostat.energyUsage());
   };
+
+  $('#cityDropDown').change( function () {
+    var city = $('#cityDropDown').val()
+    getCityURL(city, (data) => $('.cityTemp').text(Math.round(data.list[0].main.temp)));
+  })
 
   $('.temp').text(thermostat.temp);
     energyUsageColour();
